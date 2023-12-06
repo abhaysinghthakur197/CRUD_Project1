385a75866648537fe4daec05e4fd0fb58c57970e
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2023-12-06 19:15:59 +0530

    added mongodb

--- index.js
+++ index.js
@@ -9,9 +9,67 @@ const users  = require("./MOCK_DATA.json")
 
 const PORT = 8000;
 
+// {Mongoose }
+
+const mongoose  = require('mongoose');
+
+// Connect mongoose
+
+mongoose.connect('mongodb://127.0.0.1:27017/CRUD_Project').then(() => console.log("Mongodb Connected")).catch((err) => console.log("Error mongodb",err));
+
+// Mongoose Schema
+
+const userSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        require: true
+    },
+    last_name: {
+       type: String
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    gender: {
+        type: String
+    },
+    city: {
+        type: String
+    }
+})
+
+// Mongoose Modal
+
+const userModal = mongoose.model('user',userSchema)
+
+
+// mongodb post method
+
+app.post("/api/users", async (req,res) =>{
+    const body = req.body;
+
+    if(
+        !body.first_name || !body.last_name || !body.email || !body.gender
+        ) {
+            return res.status(400).json({ msg: "All fields are required.."});
+        }
+        // Creating users mongodb
+        const result = await userModal.create({
+            first_name:body.first_name,
+            last_name:body.last_name,
+            email:body.email,
+            gender:body.gender
+        });
+        console.log(result);
+        return res.status(201).json({msg:"Success"});
+});
+
 
 
 // Middleware - It is used to get the requested data in post method in json body for express.
+// the urlencoded is converting the string object into javascript object.
 app.use(express.urlencoded({extended:false}));
 
 // Routes
@@ -27,6 +85,8 @@ app.get('/users',(req,res)=>{
 
 // It is for all like app,ios,alexa, browsers etc.
 app.get('/api/users',(req,res)=>{
+
+    res.setHeader("X-Customheader","aaaa");
     return res.json(users);
 })
 
@@ -40,41 +100,41 @@ app.get('/api/users/:id',(req,res)=>{
 
 // POST Method
 
-app.post('/api/users', (req,res) => {
-    const user = req.body;
-    // Before middleware
-    // console.log(user) --> Output - undefined (Because express is not able to find the type of data that is requested. To find that we user ****Middleware****.)
+// app.post('/api/users', (req,res) => {
+//     const user = req.body;
+//     // Before middleware
+//     // console.log(user) --> Output - undefined (Because express is not able to find the type of data that is requested. To find that we user ****Middleware****.)
 
-    // After middleware use
-    // console.log(user);
+//     // After middleware use
+//     // console.log(user);
 
-    users.push({...user, id: users.length + 1});
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err,data)=>{
+//     users.push({...user, id: users.length + 1});
+//     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err,data)=>{
 
-        return res.json({status:"Success",id : users.length })
-    })
+//         return res.json({status:"Success",id : users.length })
+//     })
 
 
 
-})
+// })
 
 // PATCH Method
-app.patch('/api/users/:id',(req,res)=>{
+// app.patch('/api/users/:id',(req,res)=>{
 
-    const user = req.body;
+//     // const user = req.body;
 
-    const user = users.find((u) => u.id === user.id)
+//     // const user = users.find((u) => u.id === user.id)
 
 
-    return res.send({status :" patch pending"})
+//     return res.send({status :" patch pending"})
 
-})
+// })
 
 // DELETE Method
-app.delete('/api/users/:id',(req,res)=>{
-    return res.send({status : "delete pending"})
+// app.delete('/api/users/:id',(req,res)=>{
+//     return res.send({status : "delete pending"})
 
-})
+// })
 
 
 // We can use route also to add the api
